450c0ab3cf5299d5570249865c0d93ad234a9c4b
it-manager <itm@bridgelightels.com>
2019-05-13 18:48:45 +0900

    되돌리기: document.on(pageinit) -> document.ready()

--- wp-content/themes/2017-child/assets/js/global.js
+++ wp-content/themes/2017-child/assets/js/global.js
@@ -62,7 +62,7 @@
 	}
 
 	// HTML 페이지 준비? 땅!
-	$( document ).on( 'pageinit', function() {
+	$( document ).ready( function() {
 
 		// 페이지 로드 시 현재 페이지가 속한 서브메뉴(드롭다운 메뉴)를 미리 펼치고 있게 함
 /*		if ( $currentMenuParent.length ) {
