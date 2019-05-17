273f5977a79a858d7f0f12d8d4c7494bd5a9043b
Ethan Lee <gh0st.netdiver@gmail.com>
2019-05-17 17:43:57 +0900

    헤더 슬라이드의 텍스트 변경 (길이 축소)

--- wp-content/themes/2017-child/header.php
+++ wp-content/themes/2017-child/header.php
@@ -62,17 +62,17 @@
 				<a href="<?php echo home_url(); ?>/about/support-and-services/"><div>
 					<img src="<?php echo content_url(); ?>/uploads/2019/05/scholarship-display.jpg" />
 					<span class="bl-title">장학금을 주는 학원?!</span>
-					<span class="bl-caption">우리학원 디게 좋아요. 막 돈도 줘요. 엄청 많이요. 중3까지의 영어학업을 브릿지라잇에서 끝마치면 말이지요, 후후후... you just activated my trap card... 학생들이 매년 200만원씩 받아가요. 열심히 공부한 당신 떠나라, 고등학교로...?</span>
+					<span class="bl-caption">우리학원 디게 좋아요. 막 돈도 줘요. 중3까지의 영어학업을 브릿지라잇에서 끝마치면 말이지요, 후후후... you just activated my trap card...</span>
 				</div></a>
 				<a href="<?php echo home_url(); ?>/admission/appt-and-visit/"><div>
 					<img src="<?php echo content_url(); ?>/uploads/2019/05/certificate-pics-wall.jpg" />
 					<span class="bl-title">춘천대표 우수학원</span>
-					<span class="bl-caption">저희 로비 벽에 걸린 우수학원상입니다. 맞아요, 저희가 좀 우수해요. 우수한 강사진! 프로그램부터 학생 개개인까지 신경쓰는 원장단! 친절한 상담실장! 우수우수해요. 가을 되면 난리나요. 우수수..... 메롱</span>
+					<span class="bl-caption">저희 로비 벽에 걸린 우수학원상입니다. 맞아요, 저희가 좀 우수해요. 우수한 강사진! 가을 되면 난리나요. 우수수..... 메롱</span>
 				</div></a>
 				<a href="<?php echo home_url(); ?>/about/foundation-story/"><div>
 					<img src="<?php echo content_url(); ?>/uploads/2019/04/main-building-at-night.jpg" />
 					<span class="bl-title">Story of Establishment</span>
-					<span class="bl-caption">I'm just writing this in English so I can check how it looks. 한글도 섞어서 써봅니다. Yes, this homepage is a beta versoin. "여기저기 둘러봐준다면 유혈사태는 일어나지 않을 것입니다." by 문명)</span>
+					<span class="bl-caption">Yes, this homepage is a beta versoin. Quote: 여기저기 둘러봐준다면 유혈사태는 일어나지 않을 것입니다. by Civilization the game</span>
 				</div></a>
 			</div>
 
