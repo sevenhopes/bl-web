cfeed3dbe54e8d1076f59b5f1ed69ec031468fd6
Ethan Lee <gh0st.netdiver@gmail.com>
2019-06-13 15:32:32 +0900

    페이지 이름 변경: 학원 규정->등록 규정, 등록 규정 페이지 내용  추가

--- wp-content/themes/2017-child/header.php
+++ wp-content/themes/2017-child/header.php
@@ -73,7 +73,6 @@
 	<?php endif; ?>
 
 	<?php
-
 	/*
 	 * If a regular post or page, and not the front page, show the featured image.
 	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
