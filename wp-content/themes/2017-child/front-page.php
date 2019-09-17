e484b2e08112466be6719d3f899050c56c6f26a7
Ethan Lee <gh0st.netdiver@gmail.com>
2019-09-17 21:07:23 +0900

    프론트 페이지: 슬라이드, NEWS, EVENTS를 (json 데이터를 저장하는)게시물에서 내용을 읽어 보여줌. 즉, 나중에 해당 데이터 게시물의 내용 수정으로 front-page.php 코드를 건드리지 않고 내용 업데이트 가능.

--- wp-content/themes/2017-child/front-page.php
+++ wp-content/themes/2017-child/front-page.php
@@ -22,34 +22,21 @@ get_header(); ?>
 			</header>
 
 			<div class="bl-slides">
-				<a href="http://www.bridgelightels.com/m/about/why-bridge-light/">
+			<?php
+			$slides = json_decode( get_post_field( 'post_content', 1752 ) );
+
+			foreach ( (array) $slides as $s ) :
+			?>
+				<a href="<?php echo $s->link ?>">
 					<div class="bl-link-wrapper">
-						<img src="http://www.bridgelightels.com/m/wp-content/uploads/2019/05/header-bridge-light.jpg" />
-						<div class="bl-title"><h2>왜 브릿지라잇인가</h2></div>
-						<div class="bl-caption">영어가 어려운가요? 다리를 놓았습니다. 빛을 밝혔습니다! 따라만 오세요. 나머진 저희가 책임지겠습니다.</div>
+						<img src="<?php echo $s->img ?>" />
+						<div class="bl-title"><h2><?php echo $s->title ?></h2></div>
+						<div class="bl-caption"><?php echo $s->caption ?></div>
 					</div>
 				</a>
-				<a href="http://www.bridgelightels.com/m/about/support-and-services/">
-					<div class="bl-link-wrapper">
-						<img src="http://www.bridgelightels.com/m/wp-content/uploads/2019/05/header-scholarship-display.jpg" />
-						<div class="bl-title"><h2>장학금을 주는 학원?!</h2></div>
-						<div class="bl-caption">브릿지라잇은 학생들의 열정을 응원합니다. 미래의 리더들이 앞으로 나아가는 것을 적극 지원하고 있습니다.</div>
-					</div>
-				</a>
-				<a href="http://www.bridgelightels.com/m/admission/appt-and-visit/">
-					<div class="bl-link-wrapper">
-						<img src="http://www.bridgelightels.com/m/wp-content/uploads/2019/05/header-certificate-pics-wall.jpg" />
-						<div class="bl-title"><h2>춘천대표 우수학원으로 오세요</h2></div>
-						<div class="bl-caption">우수학원상은 감사히 받았습니다. 하지만 춘천에서 가장 많은 학생들이 다니는 영어학원으로서, 브릿지라잇은 교육으로 더 말하겠습니다.</div>
-					</div>
-				</a>
-				<a href="https://blog.naver.com/bomikimorr/">
-					<div class="bl-link-wrapper">
-						<img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/BL-inside-featured-image.png" />
-						<div class="bl-title"><h2>BTS가 좋다 Ⅱ</h2></div>
-						<div class="bl-caption">미국 땅에 살고 있는 한국인으로서, BTS가 고맙지 않다면, 거짓말이다. 김정은과 트럼프가 책상 위의 미사일 발사 버튼 운운할 때, 거리의 미국인들을 인터뷰 한 토크쇼를 보았다. 많은 미국인들이 ...</div>
-					</div>
-				</a>
+			<?php
+			endforeach;
+			?>
 			</div>
 
 			<div id="bl-front-menu">
@@ -72,7 +59,35 @@ get_header(); ?>
 					<h2 class="bl-block-title">News</h2>
 					<div class="bl-inline-link"><a href="http://www.bridgelightels.com/m/news-n-events/#news">All News</a></div>
 				</div>
-				<a href="http://www.bridgelightels.com/m/2019-fall-semester-open/">
+
+				<?php
+				if ( have_posts() ) :
+					while ( have_posts() ) :
+						the_post();
+
+						// if ( post_date ) {
+						// 	;
+						// }
+				?>
+				<a href="<?php the_permalink(); ?>">
+					<div class="bl-link-wrapper">
+						<div class="bl-block-header-media">
+							<!-- <img class="wp-image-405" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/04/summer-musical-camp-2018.jpg" alt="Summer Musical Camp" width="1200" height="675" /> -->
+							<?php the_post_thumbnail(); ?>
+						</div>
+						<div class="bl-wrap bl-block-content">
+							<h2><?php the_title(); ?></h2>
+							<p class="bl"><?php the_excerpt(); ?></p>
+						</div>
+					</div>
+				</a>
+				<?php
+					endwhile;
+				endif;
+				?>
+
+
+<!-- 				<a href="http://www.bridgelightels.com/m/2019-fall-semester-open/">
 					<div class="bl-link-wrapper">
 						<div class="bl-block-header-media">
 							<img class="wp-image-405" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/04/summer-musical-camp-2018.jpg" alt="Summer Musical Camp" width="1200" height="675" />
@@ -80,10 +95,10 @@ get_header(); ?>
 						<div class="bl-wrap bl-block-content">
 							<h2>2019 가을학기 개강 일정 및 개요</h2>
 							<p class="bl">2019년도 2학기가 개강을 알립니다! 정규 학기 뿐 아니라 할로윈, 크리스마스 등 다양한 교과외 학습활동과 겨울방학 중 영어캠프, 미국현지체험연수도 체크하세요.</p>
-							<!-- <p class="bl">단기간에 영어실력을 집.중.적으로 향상시키는 프로그램을 찾고 있나요? 그럼 브릿지라잇 <b class="bl-ue">여름 영어캠프</b>를 신청하세요! 그리고 개학일 영어수업에 씨익 웃으며 들어가세요.</p> -->
+							<!-- <p class="bl">단기간에 영어실력을 집.중.적으로 향상시키는 프로그램을 찾고 있나요? 그럼 브릿지라잇 <b class="bl-ue">여름 영어캠프</b>를 신청하세요! 그리고 개학일 영어수업에 씨익 웃으며 들어가세요.</p> --<>
 						</div>
 					</div>
-				</a>
+				</a> -->
 			</div>
 
 			<div id="bl-event" class="bl-block">
@@ -97,16 +112,15 @@ get_header(); ?>
 				// Events 글의 아이디로 그 글의 내용인 JSON 코드 파싱
 				$events = json_decode( get_post_field( 'post_content', 1697 ) );
 
-				if ( $events ) :
-					foreach ( $events as $ev ) :
-						$starttime = strtotime( $ev->{"start"} );
+				foreach ( (array) $events as $ev ) :
+					$starttime = strtotime( $ev->{"start"} );
 
-						if ( $starttime >= strtotime( "+30 days", time() ) ) :
-							break;
-						endif;
+					if ( $starttime >= strtotime( "+30 days", time() ) ) {
+						break;
+					}
 
-						if ( is_frontpage_event( $ev ) ) :
-						// echo date( "Y-m-d H:i:s", strtotime($ev->{"start"}) )." / ".date( "Y-m-d H:i:s", strtotime($ev->{"end"}) )."<br>";
+					if ( is_frontpage_event( $ev ) ) :
+					// echo date( "Y-m-d H:i:s", strtotime($ev->{"start"}) )." / ".date( "Y-m-d H:i:s", strtotime($ev->{"end"}) )."<br>";
 				?>
 					<div class="bl-event-item<?php echo $ev->{"holiday"} ? ' bl-holiday' : ( $ev->{"public"} ? '' : ' staff-event'); ?>" data-date="<?php echo $ev->{"start"} ?>">
 						<div class="bl-tear-off">
@@ -122,24 +136,15 @@ get_header(); ?>
 						</div>
 					</div>
 				<?php
-						endif;
-					endforeach;
-				else :
-					echo "앗, 아무 이벤트도 없습니다. 머쓱하네요. 머쓱타드 :p\n";
-				endif;
+					endif;
+				endforeach;
 
 				function is_frontpage_event( $ev ) {
-					$show = false;
-					if ( $ev->{"public"} && ! $ev->{"pending"} ) {
-						if ( strtotime( "+2 day", strtotime( $ev->{"start"} ) ) >= time() && strtotime( $ev->{"start"} ) <= strtotime( "+30 days", time() ) ) {
-							$show = true;
-						}
-						//  else {
-						// 	$show = strtotime( "+2 day", strtotime( $ev->{"end"} ? $ev->{"end"} : $ev->{"start"} ) ) >= time() ? true : false;
-						// }
-					}
-					// return $ev->{"public"} && ! $ev->{"pending"} && ( $ev->{"end"} ? strtotime( "-2 day", $ev->{"end"} ) >= time() : strtotime( $ev->{"start"} < time() ) );
-					return $show;
+					$public = $ev->{"public"} && ! $ev->{"pending"};
+					$recent_start = strtotime( "+2 day", strtotime( $ev->{"start"} ) ) >= time();
+					$recent_end = strtotime( $ev->{"start"} ) <= strtotime( "+30 days", time() );
+
+					return $public && $recent_start && $recent_end;
 				}
 				?>
 					<div class="bl-event-comment">* 빨강색 일정은 휴원</div>
