6363ee4107095739c5f19ea435ce8e848ea5802e
Ethan Lee <gh0st.netdiver@gmail.com>
2020-01-13 19:40:24 +0900

    이미지 스프라이트 적용; 뉴스,이벤트 읽어오는 부분 개선; 슬라이드쇼 부분 개선; 개발코드 삽입

--- wp-content/themes/2017-child/front-page.php
+++ wp-content/themes/2017-child/front-page.php
@@ -30,7 +30,7 @@ get_header(); ?>
 			?>
 				<a href="<?php echo $s->link ?>">
 					<div class="bl-link-wrapper">
-						<img src="<?php echo $s->img ?>" />
+						<img class="size-medium wp-image-<?php echo $s->attach ?>" src="<?php echo $s->src ?>" srcset="<?php echo wp_get_attachment_image_srcset( $s->attach ) ?>" sizes="100vw" />
 						<div class="bl-title"><h2><?php echo $s->title ?></h2></div>
 						<div class="bl-caption"><?php echo $s->caption ?></div>
 					</div>
@@ -41,12 +41,12 @@ get_header(); ?>
 			</div>
 
 			<div id="bl-front-menu">
-				<div><a href="tel:033-243-5757"><img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/icon-call.png" alt="전화상담&예약" /><span>문의전화</span></a></div>
-				<div><a href="http://www.bridgelightels.com/m/admission/appt-and-visit/"><img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/icon-map.png" alt="위치안내" /><span>위치안내</span></a></div>
-				<div><a href="http://www.bridgelightels.com/m/curriculum/course-overview/"><img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/icon-school.png" alt="커리큘럼" /><span>커리큘럼</span></a></div>
-				<div><a href="http://www.bridgelightels.com/m/curriculum/english-camp/"><img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/icon-devel.png" alt="방학영어캠프" /><span>영어캠프</span></a></div>
-				<div><a href="http://www.bridgelightels.com/m/curriculum/study-in-america/"><img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/icon-travel.png" alt="미국현지체험연수" /><span>미국연수</span></a></div>
-				<div><a href="http://www.bridgelightels.com/m/admission/faq/"><img src="http://www.bridgelightels.com/m/wp-content/themes/2017-child/assets/images/icon-faq.png" alt="자주묻는질문" /><span>FAQ</span></a></div>
+				<div><a href="tel:033-243-5757"><i class="bl-sp icon-call" title="전화상담&예약"></i><span>문의전화</span></a></div>
+				<div><a href="http://www.bridgelightels.com/m/admission/appt-and-visit/"><i class="bl-sp icon-map" title="위치안내"></i><span>위치안내</span></a></div>
+				<div><a href="http://www.bridgelightels.com/m/curriculum/course-overview/"><i class="bl-sp icon-book" title="커리큘럼"></i><span>커리큘럼</span></a></div>
+				<div><a href="http://www.bridgelightels.com/m/curriculum/english-camp/"><i class="bl-sp icon-devel" title="방학영어캠프"></i><span>영어캠프</span></a></div>
+				<div><a href="http://www.bridgelightels.com/m/curriculum/admission-process/"><i class="bl-sp icon-mortar" title="입학과정"></i><span>입학과정</span></a></div>
+				<div><a href="http://www.bridgelightels.com/m/admission/faq/"><i class="bl-sp icon-faq" title="자주묻는질문"></i><span>FAQ</span></a></div>
 			</div>
 
 			<div class="bl-indicator-wrapper">
@@ -62,39 +62,50 @@ get_header(); ?>
 				</div>
 
 				<?php
+				$current_raw = 0;
+				$current_ymd = '';
+				$weekago_raw = 0;
+				$weekago_ymd = '';
+
 				$query = new WP_Query( array( 'category_name' => 'news' ) );
-				if ( $query->have_posts() ) :
+				if ( ! $query->have_posts() ) :
+					echo "<p>앗, 아무 뉴스도 없습니다. 머쓱하네요. 머쓱타드 :p</p>";
+				else :
 					// 최근 글을 하나 이상 보여줌
 					$query->the_post();
-					while ( $query->have_posts() ) :
+					while ( true ) :
 				?>
 
-				<a href="<?php the_permalink(); ?>">
+				<a href="<?php the_permalink() ?>">
 					<div class="bl-news-item">
 						<div class="bl-block-header-media">
-							<?php the_post_thumbnail(); ?>
+							<?php the_post_thumbnail() ?>
 						</div>
 						<div class="bl-wrap bl-block-content">
-							<h2><?php the_title(); ?></h2>
-							<p class="bl more-link"><?php the_excerpt(); ?></p>
+							<h2><?php the_title() ?></h2>
+							<p class="bl more-link"><?php the_excerpt() ?></p>
 						</div>
 					</div>
 				</a>
 
 				<?php
+						$query->the_post();
+
+						$current_raw = strtotime( get_the_date( 'Y-m-d' ) );
+						$current_ymd = get_the_date( 'Y-m-d' );
+						$weekago_raw = time() - ( 7 * 24 * 60 * 60 );
+						$weekago_ymd = date( 'Y-m-d', $weekago_raw );
+
 						// 일주일 이상 지난 글은 보여주지 않음
-						if ( strtotime( get_the_date( 'Y-m-d' ) ) < time() - ( 7 * 24 * 60 * 60 ) ) {
+						if ( $current_raw < $weekago_raw || ! $query->have_posts() ) {
 							break;
 						}
-						$query->the_post();
 					endwhile;
 					wp_reset_postdata();
-				else :
-					echo "<p>앗, 아무 뉴스도 없습니다. 머쓱하네요. 머쓱타드 :p</p>";
 				endif;
 				?>
 
-			</div>
+			</div><!-- #bl-news -->
 
 			<div id="bl-event" class="bl-block">
 				<div class="bl-block-header">
@@ -114,6 +125,7 @@ get_header(); ?>
 						break;
 					}
 
+					// 프론트페이지에 표시될 이벤트이면, 시작일만, 또는 시작일과 종료일을 표시 (연속되지 않는 시작일과 종료일의 경우, 예로 17일, 19일 같은 경우 그 사이에 ' ~ ' 대신에 ', '를 사용)
 					if ( is_frontpage_event( $ev ) ) :
 					// echo date( "Y-m-d H:i:s", strtotime($ev->{"start"}) )." / ".date( "Y-m-d H:i:s", strtotime($ev->{"end"}) )."<br>";
 				?>
@@ -125,7 +137,7 @@ get_header(); ?>
 						<div class="bl-event-info">
 							<h2 class="e-i-title"><?php echo $ev->{"title"} ?></h2>
 							<div class="e-i-date">
-								<time itemprop="startDate" datetime="<?php echo $ev->{"start"} ?>"><?php echo bl_w2k( date( "n\월 j\일 D", $starttime ) ) ?></time><?php if ( $ev->{"end"} ) : $endtime = strtotime( $ev->{"end"} ); ?><time itemprop="endDate" datetime="<?php echo $ev->{"end"} ?>"><?php echo $starttime == strtotime( "-1 day", $endtime ) ? ', ' : ' ~ ' ?><?php echo bl_w2k( date( substr_compare( $ev->{"start"}, substr( $ev->{"end"}, 5, 2 ), 5, 2) == 0 ? "j\일 D" : "n\월 j\일 D", $endtime ) ) ?></time><?php endif; ?>
+								<time itemprop="startDate" datetime="<?php echo $ev->{"start"} ?>"><?php echo bl_w2k( date( "n\월 j\일 D", $starttime ) ) ?></time><?php if ( $ev->{"end"} ) : $endtime = strtotime( $ev->{"end"} ); ?><time itemprop="endDate" datetime="<?php echo $ev->{"end"} ?>"><?php echo $starttime == strtotime( "-1 day", $endtime ) || isset( $ev->{"separated"} ) ? ', ': ' ~ ' ?><?php echo bl_w2k( date( substr_compare( $ev->{"start"}, substr( $ev->{"end"}, 5, 2 ), 5, 2) == 0 ? "j\일 D" : "n\월 j\일 D", $endtime ) ) ?></time><?php endif; ?>
 							</div>
 							<div class="e-i-extra"><?php echo $ev->{"extra"} ?></div>
 						</div>
@@ -134,6 +146,8 @@ get_header(); ?>
 					endif;
 				endforeach;
 
+				// public(공개형) 이벤트 중 pending(확정되지 않은) 이벤트를 제외하고,
+				// 날짜가 한 달 이내인 이벤트는 true
 				function is_frontpage_event( $ev ) {
 					$public = $ev->{"public"} && ! $ev->{"pending"};
 					$recent_start = strtotime( "+2 day", strtotime( $ev->{"start"} ) ) >= time();
@@ -153,7 +167,7 @@ get_header(); ?>
 				<a href="http://www.bridgelightels.com/m/about/why-bridge-light/">
 					<div class="bl-link-wrapper">
 						<div class="bl-block-header-media">
-							<img src="http://www.bridgelightels.com/m/wp-content/uploads/2019/04/main-building-at-night.jpg" alt="" width="1200" height="675" />
+							<img class="size-medium wp-image-1091" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/04/main-building-at-night.jpg" srcset="<?php echo wp_get_attachment_image_srcset( 1091 ) ?>" sizes="100vw" alt="브릿지라잇 본관 전경" />
 						</div>
 						<div class="bl-wrap">
 							<p class="bl">브릿지라잇 어학원의 건물은 작은 영어권 도시입니다. 선생님들, 학생들 모두가 영어로 대화하는 장면이 브릿지라잇에서는 일상입니다. 가이드가 확실한 영어권 도시로 '매일' 여행오세요.</p>
@@ -169,7 +183,7 @@ get_header(); ?>
 				<a href="http://www.bridgelightels.com/m/curriculum/course-overview/">
 					<div class="bl-link-wrapper">
 						<div class="bl-block-header-media">
-							<img class="bl-center-overflow" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/09/Graphic-Roadmap-2019-09v1.jpg" alt="Bridge Light Curriculum Roadmap" width="1440" height="900" />
+							<img class="bl-center-overflow wp-image-1765" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/09/Graphic-Roadmap-2019-09v1.jpg" srcset="<?php echo wp_get_attachment_image_srcset( 1765 ) ?>" sizes="100vw" alt="브릿지라잇 커리큘럼 로드맵" />
 						</div>
 						<div class="bl-wrap">
 							<p class="bl">초등 1학년부터 중학 3학년까지를 모두 아우르는 단계별 강의들이 개설 되어 있습니다. 모든 레벨에서 영어의 전 영역인 말하기, 듣기, 읽기, 쓰기를 종합적으로 학습합니다.</p>
@@ -185,7 +199,7 @@ get_header(); ?>
 				<a href="http://www.bridgelightels.com/m/admission/admission-process/">
 					<div class="bl-link-wrapper">
 						<div class="bl-block-header-media">
-							<img class="wp-image-405" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/04/summer-musical-camp-2018.jpg" alt="Summer Musical Camp" width="1200" height="675" />
+							<img class="size-medium wp-image-802" src="http://www.bridgelightels.com/m/wp-content/uploads/2019/04/summer-musical-camp-2018.jpg" srcset="<?php echo wp_get_attachment_image_srcset( 802 ) ?>" sizes="100vw" alt="Summer Musical Camp" />
 						</div>
 						<div class="bl-wrap">
 							<p class="bl">많은 이들이 영어를 가르칩니다. 그렇지만 모든 아이들이 학원 가기를 좋아하는 건 아닙니다. 브릿지라잇의 영어수업은 지식 뿐 아니라 재미와 행복을 함께 전합니다. 신나서 가는 학원, 브릿지라잇은 그 방향으로 갑니다.</p>
@@ -198,5 +212,15 @@ get_header(); ?>
 
 	</main><!-- #main -->
 </div><!-- #primary -->
+
+<div class="bl-dev-code">
+	<?php
+		// echo $_SERVER['HTTP_USER_AGENT'] . "<br>";
+		echo '<p>c-raw: '.$current_raw.'</p>';
+		echo '<p>w-raw: '.$weekago_raw.'</p>';
+		echo '<p>c-ymd: '.$current_ymd.'</p>';
+		echo '<p>w-ymd: '.$weekago_ymd.'</p>';
+	?>
+</div>
 
 <?php get_footer();
