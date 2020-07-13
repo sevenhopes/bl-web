dedd9f82dba51cc5a5ad55392d45b44f734fd108
Ethan Lee <gh0st.netdiver@gmail.com>
2020-07-13 18:56:26 +0900

    News & Events: 이벤트 보여주는 코드 수정. (front-page.php에서 가져와 적용)

--- wp-content/themes/2017-child/home.php
+++ wp-content/themes/2017-child/home.php
@@ -74,72 +74,46 @@ get_header(); ?>
 					<h2 class="bl-block-title">Events</h2>
 				</div>
 				<div class="bl-wrap">
-
 				<?php
 				// Events 글의 아이디로 그 글의 내용인 JSON 코드 파싱
 				$events = json_decode( get_post_field( 'post_content', 2071 ) );
-				$count = 0;
 
-				// public(공개형) 이벤트 중 pending(확정되지 않은) 이벤트를 제외한 모든 이벤트는 true를 return
-				function is_displayable( $event ) {
-					$starttime = strtotime( $event->{"start"} );
-					$endtime = strtotime( $event->{"end"} );
-					$public = $event->{"public"} && ! $event->{"pending"};
-
-					return $public; // && $nearfuture;
-				}
-
-				foreach ( (array) $events as $ev ) :
-					// 표시되어야 할 이벤트인 경우만 화면 출력,
-					// 하루 이벤트이면 (시작일과 종료일이 같음) 시작일만 표시, 기간을 가진 이벤트이면 시작일과 종료일을 함께 표시
-					// 연속된 기간이 아닌 경우, 예로 시작일은 17일, 종료일은 19일, 18일은 해당 없음 같은 경우 그 사이에 ' ~ ' 대신에 ', '를 사용)
-					if ( is_displayable( $ev ) ) :
-						$count++;
-						$one_day = $ev->{"start"} == $ev->{"end"};
+				if ( $events ) :
+					foreach ( $events as $ev ) :
 						$starttime = strtotime( $ev->{"start"} );
-						$endtime = strtotime( $ev->{"end"} );
-						// echo date( "Y-m-d H:i:s", strtotime($ev->{"start"}) )." / ".date( "Y-m-d H:i:s", strtotime($ev->{"end"}) )."<br>";
+
+						/* 공개 설정된 이벤트만 보여줌
+						 * 날짜가 정해지지 않은 경우(pending) t-o-day의 텍스트를 '?'로 바꾸고, <time>태그 텍스트에서 날짜 표시 대신 '0월 중'으로 표시
+						 * <time> 태그 부분에 대해:
+						 *   bl_w2k는 영어 요일을 한글 요일로 바꾸어 줌 (예. Wed -> 수)
+						 *   <time>내의 if문은 이벤트가 1일 이상인 경우(끝날짜가 존재하는 경우) 끝날짜 부분 추가
+						 *   시작날짜와 끝날짜가 하루 차이면(딱 2일 이벤트의 경우), 날짜 사이에 '~' 대신 ',' 쉼표를 표시. 3일 이상 이벤트만 '~' 표시
+						 *   1일 이상 이벤트 중에 다음달로 끝날짜가 넘어가는 경우(예. 1월 30일 ~ 2월 5일)에만 끝날짜의 월 표시
+						 */
+						if ( $ev->{"public"} ) :
 				?>
-					<div class="bl-event-item<?php echo $ev->{"holiday"} ? ' bl-holiday' : ''; ?>" data-date="<?php echo $ev->{"start"} ?>">
+					<div class="bl-event-item<?php echo $ev->{"holiday"} ? ' bl-holiday' : ( $ev->{"public"} ? '' : ' staff-event'); ?>" data-date="<?php echo $ev->{"start"} ?>">
 						<div class="bl-tear-off">
-							<div class="t-o-day"><?php echo date( "j", $starttime ) ?></div>
+							<div class="t-o-day"><?php echo $ev->{"pending"} ? '?' : date( "j", $starttime ) ?></div>
 							<div class="t-o-mon"><?php echo date( "M", $starttime ) ?></div>
 						</div>
 						<div class="bl-event-info">
 							<h2 class="e-i-title"><?php echo $ev->{"title"} ?></h2>
 							<div class="e-i-date">
-								<time itemprop="startDate" datetime="<?php echo $ev->{"start"} ?>"><?php echo bl_w2k( date( "n\월 j\일 D", $starttime ) ) ?></time><?php if ( ! $one_day ) : $endtime = strtotime( $ev->{"end"} ); ?><time itemprop="endDate" datetime="<?php echo $ev->{"end"} ?>"><?php echo $starttime == strtotime( "-1 day", $endtime ) || isset( $ev->{"separated"} ) ? ', ': ' ~ ' ?><?php echo bl_w2k( date( substr_compare( $ev->{"start"}, substr( $ev->{"end"}, 5, 2 ), 5, 2) == 0 ? "j\일 D" : "n\월 j\일 D", $endtime ) ) ?></time><?php endif; ?>
+								<time itemprop="startDate" datetime="<?php echo $ev->{"start"} ?>"><?php echo bl_w2k( date( $ev->{"pending"} ? "n\월 \중" : "n\월 j\일 D", $starttime ) ) ?></time><?php if ( $ev->{"end"} && ! $ev->{'pending'} ) : $endtime = strtotime( $ev->{"end"} ); ?><time itemprop="endDate" datetime="<?php echo $ev->{"end"} ?>"><?php echo $starttime == strtotime( "-1 day", $endtime ) ? ', ' : ' ~ ' ?><?php echo bl_w2k( date( substr_compare( $ev->{"start"}, substr( $ev->{"end"}, 5, 2 ), 5, 2) == 0 ? "j\일 D" : "n\월 j\일 D", $endtime ) ) ?></time><?php endif; ?>
 							</div>
 							<div class="e-i-extra"><?php echo $ev->{"extra"} ?></div>
 						</div>
 					</div>
 				<?php
-					endif;
-				endforeach;
-
-				if ( $count == 0 ) :
-					$today = date( "Y-m-d" );
-					$now = time();
-				?>
-					<div class="bl-event-item" data-date="<?php echo $today ?>">
-						<div class="bl-tear-off">
-							<div class="t-o-day"><?php echo date( "j" ) ?></div>
-							<div class="t-o-mon"><?php echo date( "M" ) ?></div>
-						</div>
-						<div class="bl-event-info">
-							<h2 class="e-i-title">오늘도 영어열공 중</h2>
-							<div class="e-i-date">
-								<time itemprop="startDate" datetime="<?php echo $today ?>"><?php echo bl_w2k( date( "n\월 j\일 D", $now ) ) ?></time>
-							</div>
-							<div class="e-i-extra"></div>
-						</div>
-					</div>
-				<?php
+						endif;
+					endforeach;
+				else :
+					echo "<p>앗, 아무 이벤트도 없습니다. 머쓱하네요. 머쓱타드 :p</p>";
 				endif;
 				?>
-					<div class="bl-event-comment"><span class="bl-red">* 빨강색 일정은 휴원</span></div>
 				</div>
-			</div><!-- #events -->
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
